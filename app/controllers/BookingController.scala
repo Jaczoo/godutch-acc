c59ab17d3134cd49b64bcc52ef04450c0d8ef4b1
zoltanmaric <zoltan.maric@discavo.com>
2016-03-06 00:53:33 +0100

    create endpoint for listing all bookings

--- app/controllers/BookingController.scala
+++ app/controllers/BookingController.scala
@@ -1,8 +1,11 @@
 package controllers
 
+import anorm.SqlParser._
 import anorm._
+import controllers.BookingModels._
 import play.api.Play.current
 import play.api.db.DB
+import play.api.libs.json.{Format, Json}
 import play.api.mvc.{Action, Controller}
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -13,7 +16,7 @@ import scala.concurrent.Future
   */
 object BookingController extends Controller {
 
-  def createOffer(listingId: String, unitId: String, checkin: String, checkout: String, owner: String) = Action.async(parse.json) {
+  def createBooking(listingId: String, unitId: String, checkin: String, checkout: String, owner: String) = Action.async(parse.json) {
     implicit request =>
       request.body.validate[List[String]].asEither match {
         case Right(friends) =>
@@ -23,6 +26,17 @@ object BookingController extends Controller {
         case Left(error) => Future.successful(BadRequest(error.toString))
       }
   }
+
+  def getBookings(user: String) = Action.async {
+    val groupBookings = for {
+      bookingsWithId <- getUserBookings(user)
+      groupBookings <- Future.traverse(bookingsWithId)(getGroupBooking)
+    } yield groupBookings
+    groupBookings.map(Json.toJson(_)).map(Ok(_))
+  }
+
+  private def getGroupBooking(bookingWithId: BookingWithId): Future[GroupBooking] =
+    getCommitments(bookingWithId.id).map(GroupBooking(bookingWithId.booking, _))
 
   private def createOfferOnly(listingId: String, unitId: String, checkin: String, checkout: String): Future[Long] = Future {
     DB.withConnection { implicit connection =>
@@ -39,6 +53,39 @@ object BookingController extends Controller {
 
   private def assignUser(offerId: Long, isOwner: Boolean)(user: String): Unit = DB.withConnection { implicit connection =>
     SQL"""INSERT INTO user_offers (offer_id, user_email, committed, seen)
-           values ($offerId, $user, $isOwner, $isOwner)""".execute ()
+           values ($offerId, $user, $isOwner, $isOwner)""".execute()
+  }
+
+  private def getUserBookings(user: String): Future[List[BookingWithId]] = Future {
+    DB.withConnection { implicit connection =>
+      SQL"""SELECT offers.id, listing_id, unit_id, checkin, checkout
+            FROM offers JOIN user_offers on offers.id = offer_id
+            WHERE user_email = $user""".as(bookingWithIdParser.*)
+    }
+  }
+
+  private def getCommitments(offerId: Long): Future[List[Commitment]] = Future {
+    DB.withConnection { implicit connection =>
+      SQL"""SELECT user_email, committed FROM user_offers where offer_id = $offerId""".as(commitmentParser.*)
+    }
+  }
+}
+
+object BookingModels {
+  case class Booking(listingId: String, unitId: String, checkin: String, checkout: String)
+  case class BookingWithId(id: Long, booking: Booking)
+  case class Commitment(user: String, committed: Boolean)
+  case class GroupBooking(booking: Booking, commitments: List[Commitment])
+
+  implicit val bookingFormat: Format[Booking] = Json.format[Booking]
+  implicit val commitmentFormat: Format[Commitment] = Json.format[Commitment]
+  implicit val groupBookingFormat: Format[GroupBooking] = Json.format[GroupBooking]
+
+  val bookingWithIdParser = long("id") ~ str("listing_id") ~ str("unit_id") ~ str("checkin") ~ str("checkout") map {
+    case id ~ listingId ~ unitId ~ checkin ~ checkout => BookingWithId(id, Booking(listingId, unitId, checkin, checkout))
+  }
+
+  val commitmentParser = str("user_email") ~ bool("committed") map {
+    case user ~ committed => Commitment(user, committed)
   }
 }
