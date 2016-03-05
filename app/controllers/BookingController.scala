bf05d4f8a1d8af572494926beee41af6b0d63c64
zoltanmaric <zoltan.maric@discavo.com>
2016-03-05 21:29:58 +0100

    add friends support to create offer endpoint

--- app/controllers/BookingController.scala
+++ app/controllers/BookingController.scala
@@ -13,9 +13,32 @@ import scala.concurrent.Future
   */
 object BookingController extends Controller {
 
-  def createOffer(listingId: String, unitId: String, checkin: String, checkout: String) = Action.async(Future(DB.withConnection { implicit connection =>
-    val id = SQL"""INSERT INTO offers (listing_id, unit_id, checkin, checkout)
+  def createOffer(listingId: String, unitId: String, checkin: String, checkout: String, owner: String) = Action.async(parse.json) {
+    implicit request =>
+      request.body.validate[List[String]].asEither match {
+        case Right(friends) =>
+          createOfferOnly(listingId, unitId, checkin, checkout)
+            .flatMap(assignUsers(owner, friends))
+            .map(_ => Ok)
+        case Left(error) => Future.successful(BadRequest(error.toString))
+      }
+  }
+
+  private def createOfferOnly(listingId: String, unitId: String, checkin: String, checkout: String): Future[Long] = Future {
+    DB.withConnection { implicit connection =>
+      SQL"""INSERT INTO offers (listing_id, unit_id, checkin, checkout)
          values ($listingId, $unitId, $checkin, $checkout)""".executeInsert[Option[Long]]()
-    Ok(id.get.toString)
-  }))
+        .getOrElse(throw new RuntimeException(s"error creating offer $listingId, $unitId, $checkin, $checkout"))
+    }
+  }
+
+  private def assignUsers(owner: String, friends: List[String])(offerId: Long): Future[Unit] = Future {
+    assignUser(offerId, isOwner = true)(owner)
+    friends.foreach(assignUser(offerId, isOwner = false))
+  }
+
+  private def assignUser(offerId: Long, isOwner: Boolean)(user: String): Unit = DB.withConnection { implicit connection =>
+    SQL"""INSERT INTO user_offers (offer_id, user_email, committed, seen)
+           values ($offerId, $user, $isOwner, $isOwner)""".execute ()
+  }
 }
