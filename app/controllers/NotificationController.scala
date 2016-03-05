e412e8750881e377a3018afd57b1b31130e8066e
zoltanmaric <zoltan.maric@discavo.com>
2016-03-05 23:26:23 +0100

    add marking a notification read

--- app/controllers/NotificationController.scala
+++ app/controllers/NotificationController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import anorm.SqlParser._
 import anorm._
+import play.api.Logger
 import play.api.Play.current
 import play.api.db.DB
 import play.api.mvc.{Action, Controller}
@@ -14,9 +15,13 @@ import scala.concurrent.Future
   */
 object NotificationController extends Controller {
   def getNotification(user: String) = Action.async {
-    getUnseenOfferId(user).map {
-      case Some(id) => Ok(id.toString)
-      case None => NoContent
+    getUnseenOfferId(user).map(_.fold(NoContent)(id => Ok(id.toString)))
+  }
+
+  def setNotificationSeen(user: String, offerId: Long) = Action.async {
+    setOfferSeen(user, offerId).map { updated =>
+      Logger.info(s"Setting notification seen for $user id $offerId updated $updated rows.")
+      if (updated > 0) Ok else NotFound(s"No offer found for combination $user offer ID $offerId")
     }
   }
 
@@ -25,6 +30,12 @@ object NotificationController extends Controller {
       SQL"""SELECT offer_id FROM user_offers WHERE user_email = $user AND seen = FALSE LIMIT 1"""
         .executeQuery()
         .as(scalar[Long].singleOpt)
+    }
+  }
+
+  private def setOfferSeen(user: String, offerId: Long): Future[Int] = Future {
+    DB.withConnection { implicit connection =>
+      SQL"""UPDATE user_offers SET seen = TRUE WHERE user_email = $user AND offer_id = $offerId""".executeUpdate()
     }
   }
 
